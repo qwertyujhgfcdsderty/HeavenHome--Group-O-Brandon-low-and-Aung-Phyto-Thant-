0f46897dc0cfc4c534faaaa5d0296e0b112dd927
brand <brand@DESKTOP-TMNT40L>
2024-06-13 14:05:07 +0800

    material product

---
+++ WebApplication HeavenHome/Models/Material_Product.cs
@@ -0,0 +1,11 @@
+﻿namespace HeavenHome.Models
+{
+    public class Material_Product
+    {
+        public int MaterialId { get; set; }
+        public Material Material { get; set; }
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
