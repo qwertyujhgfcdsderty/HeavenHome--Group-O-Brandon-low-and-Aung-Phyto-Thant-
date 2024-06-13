13e954ba7d2c12b8963ff7ec734f03c8c231ce11
brand <brand@DESKTOP-TMNT40L>
2024-06-13 14:08:44 +0800

    Appddcontext

---
+++ WebApplication HeavenHome/Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using HeavenHome.Models;
+using Microsoft.EntityFrameworkCore;
+namespace HeavenHome.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {   
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Material_Product>().HasKey(am => new
+            {
+                am.MaterialId,
+                am.ProductId,
+            });
+
+            modelBuilder.Entity<Material_Product>().HasOne(m => m.Product).WithMany(am => am.Materials_Products).HasForeignKey(m  => m.MaterialId);
+            modelBuilder.Entity<Material_Product>().HasOne(m => m.Product).WithMany(am => am.Materials_Products).HasForeignKey(m  => m.ProductId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Material> Materials { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Material_Product> Materials_Products { get; set; }
+        public DbSet<Company> Companies { get; set; }
+    }
+}
