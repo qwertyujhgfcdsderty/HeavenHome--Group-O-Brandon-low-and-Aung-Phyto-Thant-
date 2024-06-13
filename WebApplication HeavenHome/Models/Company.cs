3d75427ba323ec610c3d4cfe096d9895a538e45b
brand <brand@DESKTOP-TMNT40L>
2024-06-13 14:02:31 +0800

    this is about company model

---
+++ WebApplication HeavenHome/Models/Company.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HeavenHome.Models
+{
+    public class Company
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Logo { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        //relationships
+        public List<Company> Companies { get; set; }
+    }
+}
