e206d20c4902fa61f4b2415b27d3076a74d02307
brand <brand@DESKTOP-TMNT40L>
2024-06-13 14:06:25 +0800

    product

---
+++ WebApplication HeavenHome/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using HeavenHome.Data.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace HeavenHome.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string ImageURL { get; set; }
+        public ProductCategory ProductCategory { get; set; }
+
+        //relationships
+        public List<Material_Product> Materials_Products { get; set; }
+
+        //company
+        public Company Company { get; set; }
+    }
+}
