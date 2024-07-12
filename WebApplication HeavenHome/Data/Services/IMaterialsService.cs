0da8bd0e6744286902056454646f78a2a06fdad3
brand <brand@DESKTOP-TMNT40L>
2024-07-12 14:52:41 +0800

    add Material service

---
+++ WebApplication HeavenHome/Data/Services/IMaterialsService.cs
@@ -0,0 +1,13 @@
+﻿using HeavenHome.Models;
+
+namespace HeavenHome.Data.Services
+{
+    public interface IMaterialsService
+    {
+        Task<IEnumerable<Material>> GetAll();
+        Material GetById(int id);
+        void Add(Material material);
+        Material Update(int id, Material newMaterial);
+        void Delete(int id);
+    }
+}
---
+++ WebApplication HeavenHome/Data/Services/MaterialsService.cs
@@ -0,0 +1,41 @@
+﻿using HeavenHome.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HeavenHome.Data.Services
+{
+    public class MaterialsService : IMaterialsService
+    {
+        private readonly AppDbContext _context;
+
+        public MaterialsService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public void Add(Material material)
+        {
+            _context.Materials.Add(material);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Material>> GetAll()
+        {
+            var result = await _context.Materials.ToListAsync();
+            return result;
+        }
+
+        public Material GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Material Update(int id, Material newMaterial)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
