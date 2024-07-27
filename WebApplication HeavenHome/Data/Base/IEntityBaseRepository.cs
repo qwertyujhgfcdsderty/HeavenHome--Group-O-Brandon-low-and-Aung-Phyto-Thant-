d04ae9e7e04f23c75ec4c17abd38675943db9b9b
brand <brand@DESKTOP-TMNT40L>
2024-07-28 01:33:50 +0800

    adding IEntityBaseRepository file

---
+++ WebApplication HeavenHome/Data/Base/IEntityBaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using HeavenHome.Models;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace HeavenHome.Data.Base
+{
+    public interface IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> GetAllAsync(params Expression<Func<T, object>>[] includeProperties);
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(int id, T entity);
+        Task DeleteAsync(int id);
+    }
+}
