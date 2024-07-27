a89df0d965eac0db186caeb4be90dcf31fa86dae
brand <brand@DESKTOP-TMNT40L>
2024-07-28 01:36:25 +0800

    new services configuration

--- WebApplication HeavenHome/Program.cs
+++ WebApplication HeavenHome/Program.cs
@@ -1,9 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using HeavenHome.Data;
+using HeavenHome.Data.Services;
+using WebApplication_HeavenHome.Data.Services;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure DbContext with SQL Server
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"), sqlOptions =>
+        sqlOptions.CommandTimeout(60)));
+
+// Services configuration
+builder.Services.AddScoped<IMaterialsService, MaterialsService>();
+builder.Services.AddScoped<ICompaniesService, CompaniesService>();
+builder.Services.AddScoped<IProductsService, ProductsService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -12,6 +28,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -23,5 +40,9 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+//seed database
+AppDbInitializer.seed(app);
 
 app.Run();
